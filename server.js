00d8a47aff4dfedb0a14079c9e578e14ebbc6f00
David Pom <davidpom@gmail.com>
2019-08-05 11:08:19 -0400

    tentative sql schema added

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.set('view engine', 'ejs');
 
 
 
@@ -18,6 +19,29 @@ require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 
+var mysql = require("mysql");
+
+// MySQL stuff
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "confercloud_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+
+
+// listening:
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
