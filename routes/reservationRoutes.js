e13d291769e0482d2ce32fe7b5315164b1f37477
Dave <42557494+DavidPomarede@users.noreply.github.com>
2019-08-17 19:37:27 -0400

    Crud routes for reservations and rooms

---
+++ routes/reservationRoutes.js
@@ -0,0 +1,82 @@
+
+
+
+module.exports = function(app) {
+
+
+    // CRUD stuff for "Reservation" tables:
+  
+    app.get("/api/Reservations/", function(req, res) {
+      db.Reservation.findAll({})
+        .then(function(dbReservation) {
+          res.json(dbReservation);
+        });
+    });
+  
+    app.get("/api/Reservations/type/:ReservationType", function(req, res) {
+      db.Reservation.findAll({
+        where: {
+          ReservationType: req.params.type
+        }
+      })
+        .then(function(dbReservation) {
+          res.json(dbReservation);
+        });
+    });
+  
+    app.get("/api/Reservations/:id", function(req, res) {
+      db.Reservation.findOne({
+        where: {
+          id: req.params.id
+        }
+      })
+        .then(function(dbReservation) {
+          res.json(dbReservation);
+        });
+    });
+  
+    app.post("/api/Reservations", function(req, res) {
+      console.log(req.body);
+      db.Reservation.create({
+        ReservationName: req.body.ReservationName,
+        ReservationCapacity: req.body.ReservationCapacity,
+        ReservationURL: req.body.ReservationURL,
+        ReservationType: req.body.ReservationType,
+        address1: req.body.address1,
+        address2: req.body.address2,
+        city: req.body.city,
+        state_us: req.body.state_us,
+        zip: req.body.zip,
+        contactPhone: req.body.contactPhone,
+        ownerID: req.user.email,
+        hourlyRate: req.body.hourlyRate
+      })
+        .then(function(dbReservation) {
+          res.json(dbReservation);
+        });
+    });
+  
+    app.delete("/api/Reservations/:id", function(req, res) {
+      db.Reservation.destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+        .then(function(dbReservation) {
+          res.json(dbReservation);
+        });
+    });
+  
+    app.put("/api/Reservations", function(req, res) {
+      db.Reservation.update(req.body,
+        {
+          where: {
+            id: req.body.id
+          }
+        })
+        .then(function(dbReservation) {
+          res.json(dbReservation);
+        });
+    });
+  
+  };
---
+++ routes/roomsRoutes.js
@@ -0,0 +1,82 @@
+
+
+
+module.exports = function(app) {
+
+
+  // CRUD stuff for "Room" tables:
+
+  app.get("/api/rooms/", function(req, res) {
+    db.Room.findAll({})
+      .then(function(dbRoom) {
+        res.json(dbRoom);
+      });
+  });
+
+  app.get("/api/rooms/type/:roomType", function(req, res) {
+    db.Room.findAll({
+      where: {
+        roomType: req.params.type
+      }
+    })
+      .then(function(dbRoom) {
+        res.json(dbRoom);
+      });
+  });
+
+  app.get("/api/rooms/:id", function(req, res) {
+    db.Room.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(function(dbRoom) {
+        res.json(dbRoom);
+      });
+  });
+
+  app.post("/api/rooms", function(req, res) {
+    console.log(req.body);
+    db.Room.create({
+      roomName: req.body.roomName,
+      roomCapacity: req.body.roomCapacity,
+      roomURL: req.body.roomURL,
+      roomType: req.body.roomType,
+      address1: req.body.address1,
+      address2: req.body.address2,
+      city: req.body.city,
+      state_us: req.body.state_us,
+      zip: req.body.zip,
+      contactPhone: req.body.contactPhone,
+      ownerID: req.user.email,
+      hourlyRate: req.body.hourlyRate
+    })
+      .then(function(dbRoom) {
+        res.json(dbRoom);
+      });
+  });
+
+  app.delete("/api/rooms/:id", function(req, res) {
+    db.Room.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(function(dbRoom) {
+        res.json(dbRoom);
+      });
+  });
+
+  app.put("/api/rooms", function(req, res) {
+    db.Room.update(req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      })
+      .then(function(dbRoom) {
+        res.json(dbRoom);
+      });
+  });
+
+};
