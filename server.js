f7c0638f5b81ff50f552f18342eb73003e4bc1ac
Aaron Keisler <amkeisler@gmail.com>
2019-08-17 10:06:27 -0400

    remove database config and update basic routing

--- server.js
+++ server.js
@@ -1,45 +1,15 @@
-
-
 var express = require("express");
-
-
 var app = express();
-
 
 var PORT = process.env.PORT || 8080;
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.set('view engine', 'ejs');
 
+app.set("view engine", "ejs");
 
-
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
-
-
-var mysql = require("mysql");
-
-// MySQL stuff
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "confercloud_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-
+app.use("/api", "/routes/apiRoutes");
+app.use("/", "/routes/viewRoutes");
 
 // listening:
 
