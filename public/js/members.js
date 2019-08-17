d34b705d97b2858dcd227f0e471fc4d3e17f79a4
Aaron Keisler <amkeisler@gmail.com>
2019-08-17 13:34:52 -0400

    adjust member-name comment

--- public/js/members.js
+++ public/js/members.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
-
-  
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(function(data) {
-    $(".member-name").text(data.first);
-    $("#login").css("display", "none")
-    $("#signup").css("display", "none")
-    $("#features").css("display", "none")
+    // $(".member-name").text(data);
+    $("#login").css("display", "none");
+    $("#signup").css("display", "none");
+    $("#features").css("display", "none");
   });
 });
