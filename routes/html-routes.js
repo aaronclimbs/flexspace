5f7e44615c0e66860630dfa71d7fd8b9e0ef8f0c
Kevin Steele <kevdsteele@gmail.com>
2019-08-17 14:23:04 -0400

    see below
    Fixing all of Aarons errors

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,16 +8,16 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
     var ejsObj = { pageTitle: "FlexSpace" };
-    res.render("/pages/home", ejsObj);
+    res.render("pages/home", ejsObj);
   });
 
-  app.get("signup", function(req, res) {
+  app.get("/signup", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     }
-    var ejsObj = {};
-    res.render("/pages/signup", ejsObj);
+    var ejsObj = {pageTitle: "Signup"};
+    res.render("pages/signup", ejsObj);
   });
 
   app.get("/login", function(req, res) {
@@ -25,21 +25,28 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    var ejsObj = {};
-    res.render("/pages/login", ejsObj);
+    var ejsObj = {pageTitle: "Login"};
+    res.render("pages/login", ejsObj);
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
+
+    var ejsObj ={
+      pageTitle: "Members"
+
+
+    }
     // TODO: Check if user is admin/renter/owner and return appropriate data
-    db.Rooms.findAll({ where: { user_id: req.user.id } }).then(result => {
+   /* db.Rooms.findAll({ where: { user_id: req.user.id } }).then(result => {
       var ejsObj = {
+        pageTitle: "Members",
         user: req.user,
         name: req.user.first,
         rooms: result[0]
-      };
-      res.render("members", ejsObj);
+      };*/
+      res.render("pages/members", ejsObj);
     });
-  });
+  //});
 };
