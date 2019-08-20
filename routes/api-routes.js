bc15cab540b36b0f588d50d8720ea4592e100fc2
davidpom <davidpom@gmail.com>
2019-08-20 19:24:58 -0400

    working nested associations

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -97,31 +97,24 @@ module.exports = function(app) {
       });
     });
 
-    app.get("/api/reservationsRoom", function(req, res) {
-      // Here we add an "include" property to our options in our findOne query
-      // We set the value to an array of the models we want to include in a left outer join
-      // In this case, just db.Author
-      console.log(req.user.id);
-      db.Reservation.findAll({
-        where: {
-          userID: req.user.id
-        },
-        include: [db.Room]
-      }).then(function(dbRes) {
-        res.json(dbRes);
-      });
-    });
+//DISPLAYING RESERVATIONS WITH ROOMS AND ROOM OWNER:
 
-    app.get("/api/reservationsUser", function(req, res) {
-      // Here we add an "include" property to our options in our findOne query
-      // We set the value to an array of the models we want to include in a left outer join
-      // In this case, just db.Author
+    app.get("/api/reservations", function(req, res) {
       console.log(req.user.id);
       db.Reservation.findAll({
         where: {
           userID: req.user.id
         },
-        include: [db.User]
+        include: [
+          {
+            model: db.Room,
+            include: [
+              {
+                model: db.User
+              }
+            ]
+          }
+        ]
       }).then(function(dbRes) {
         res.json(dbRes);
       });
