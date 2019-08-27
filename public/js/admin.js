2c2f61926ca368c3f6b64c460bb2084fb2e67154
Aaron Keisler <amkeisler@gmail.com>
2019-08-26 23:50:05 -0400

    add admin delete routes

--- public/js/admin.js
+++ public/js/admin.js
@@ -16,51 +16,73 @@ deleteReservations.forEach(btn => {
 
 // functions
 function userDelete(e) {
-  const id = e.target.dataset.id;
-  $.ajax({
-    method: "DELETE",
-    url: "api/reservations/" + id
-  }).then(function(err) {
-    if (err) {
-      console.log("error deleting reservations");
-    }
-    console.log("deleted RESERVATIONS from roomID:" + id);
-    $.ajax({
-      method: "DELETE",
-      url: "api/rooms/" + id
-    }).then(function(err) {
-      if (err) {
-        console.log("error deleting rooms");
-      }
-      console.log("deleted Rooms from userID:" + id);
+  console.log("Delete clicked");
+  const userId = e.target.dataset.id;
+  // get users room(s)
+  $.get(`/api/admin/user/${userId}/rooms`, resdata => {
+    console.log("user's rooms are:" + JSON.stringify(resdata));
+    return resdata;
+  })
+    .then(results => {
+      results.forEach(room => {
+        $.get("/api/reservationsbyroom/" + room.id, function(resdata) {
+          console.log("Reservation data length is " + resdata.length);
+          if (resdata.length !== 0) {
+            for (let i = 0; i < resdata.length; i++) {
+              $.ajax({
+                method: "DELETE",
+                url: "api/reservations/" + resdata[i].id
+              }).then(function(result) {
+                console.log("Reservation ID deleted was " + resdata[i].id);
+              });
+            }
+          }
+          $.ajax({
+            method: "DELETE",
+            url: "api/rooms/" + room.id
+          }).then(function() {
+            console.log("Room ID deleted was" + room.id);
+          });
+        });
+      });
+    })
+    .then(() => {
+      console.log(userId);
       $.ajax({
         method: "DELETE",
-        url: "api/rooms/" + id
-      }).then(() => {
-        console.log("Deleted successfully");
+        url: "api/users/" + userId
+      }).then(function() {
+        console.log("User ID deleted was:" + userId);
         location.reload(true);
       });
     });
-  });
+  /* Get the reservation for the room */
 }
 
 function roomDelete(e) {
-  const id = e.target.dataset.id;
-  $.ajax({
-    method: "DELETE",
-    url: "api/reservations/" + id
-  }).then(function(err) {
-    if (err) {
-      console.log("error deleting reservations");
+  console.log("Delete clicked");
+  e.preventDefault();
+  var roomid = e.target.dataset.id;
+  /* Get the reservation for the room */
+  $.get("/api/reservationsbyroom/" + roomid, function(resdata) {
+    console.log("Reservation data length is " + resdata.length);
+    if (resdata.length !== 0) {
+      for (let i = 0; i < resdata.length; i++) {
+        $.ajax({
+          method: "DELETE",
+          url: "api/reservations/" + resdata[i].id
+        }).then(function(result) {
+          console.log("Reservation ID deleted was " + result.id);
+        });
+      }
     }
-    console.log("deleted RESERVATIONS from roomID:" + id);
     $.ajax({
       method: "DELETE",
-      url: "api/rooms/" + id
+      url: "api/rooms/" + roomid
     }).then(function() {
-      console.log("Room Id " + id);
-      location.reload(true);
+      console.log("Room ID deleted was" + roomid);
     });
+    location.reload(true);
   });
 }
 
