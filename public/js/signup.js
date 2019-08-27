89f020d648a692522c31f30ad1fa30be75c90d44
Aaron Keisler <amkeisler@gmail.com>
2019-08-27 10:34:51 -0400

    additions to signup re validation

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-
-  $("#logout").hide()
+  $("#logout").hide();
   $("#myDashboard").hide();
   // Getting references to our form and input
+  var signupBtn = $(".signup-btn");
   var signUpForm = $("form.signup");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
@@ -16,12 +16,21 @@ $(document).ready(function() {
   var phoneInput = $("input#phone-input");
   var secQuestionInput = $("#sec-question-input");
   var secAnswerInput = $("input#sec-question-answer");
- 
+
   // When the signup button is clicked, we validate the email and password are not blank
-  signUpForm.on("submit", function(event) {
+  signupBtn.on("click", function(event) {
+    const inputs = Array.from(document.querySelectorAll(".form-control"));
+    if (!!inputs.filter(input => input.classList.includes("is-invalid"))) {
+      event.preventDefault();
+      event.stopPropogation();
+      return false;
+    }
+    // if (!userData.email || !userData.password) {
+    //   return;
+    // }
     $("#loading").fadeIn();
 
-    console.log("Signup clicked")
+    console.log("Signup clicked");
     event.preventDefault();
     var userData = {
       email: emailInput.val().trim(),
@@ -33,20 +42,26 @@ $(document).ready(function() {
       city: cityInput.val().trim(),
       state: stateInput.val().trim(),
       zip: zipInput.val().trim(),
-      phone:phoneInput.val().trim(),
-      secQuestion:secQuestionInput.val().trim(),
-      secAnswer:secAnswerInput.val().trim(),
-
-        
-
-
+      phone: phoneInput.val().trim(),
+      secQuestion: secQuestionInput.val().trim(),
+      secAnswer: secAnswerInput.val().trim()
     };
 
-    if (!userData.email || !userData.password) {
-      return;
-    }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password, userData.first, userData.last,userData.address,userData.address2, userData.city,userData.state,userData.zip,userData.phone, userData.secQuestion, userData.secAnswer);
+    signUpUser(
+      userData.email,
+      userData.password,
+      userData.first,
+      userData.last,
+      userData.address,
+      userData.address2,
+      userData.city,
+      userData.state,
+      userData.zip,
+      userData.phone,
+      userData.secQuestion,
+      userData.secAnswer
+    );
     emailInput.val("");
     passwordInput.val("");
     firstInput.val("");
@@ -63,29 +78,96 @@ $(document).ready(function() {
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(email, password, first, last, address, address2, city, state, zip, phone, secQuestion, secAnswer) {
+  function signUpUser(
+    email,
+    password,
+    first,
+    last,
+    address,
+    address2,
+    city,
+    state,
+    zip,
+    phone,
+    secQuestion,
+    secAnswer
+  ) {
     $.post("/api/signup", {
       email: email,
       password: password,
-      first:first,
-      last:last,
-      address:address,
-      address2:address2,
-      city:city,
-      state:state,
-      zip:zip,
-      phone:phone,
-      secQuestion:secQuestion,
-      secAnswer:secAnswer
-
-    }).then(function(data) {
-      window.location.replace(data);
-      // If there's an error, handle it by throwing up a bootstrap alert
-    }).catch(handleLoginErr);
+      first: first,
+      last: last,
+      address: address,
+      address2: address2,
+      city: city,
+      state: state,
+      zip: zip,
+      phone: phone,
+      secQuestion: secQuestion,
+      secAnswer: secAnswer
+    })
+      .then(function(data) {
+        window.location.replace(data);
+        // If there's an error, handle it by throwing up a bootstrap alert
+      })
+      .catch(handleLoginErr);
   }
 
   function handleLoginErr(err) {
     $("#alert").text(err.responseJSON);
     $("#alert").fadeIn(500);
   }
+});
+
+var strength = {
+  0: "Worst",
+  1: "Bad",
+  2: "Weak",
+  3: "Good",
+  4: "Strong"
+};
+
+const passwordConfirm = document.querySelector("#confirm-password-input");
+var password = document.getElementById("password-input");
+var meter = document.getElementById("password-strength-meter");
+var text = document.getElementById("password-strength-text");
+
+password.addEventListener("input", function() {
+  var val = password.value;
+  var result = zxcvbn(val);
+
+  // Update the password strength meter
+  meter.value = result.score;
+
+  // Update the text indicator
+  if (val !== "") {
+    text.innerHTML = "Strength: " + strength[result.score];
+  } else {
+    text.innerHTML = "";
+  }
+});
+
+passwordConfirm.addEventListener("blur", e => {
+  if (password.value !== passwordConfirm.value || !password.value) {
+    console.log("Passwords do not match!");
+    passwordConfirm.classList.add("is-invalid");
+  } else {
+    console.log("Passwords match!");
+    passwordConfirm.classList.remove("is-invalid");
+    passwordConfirm.classList.add("is-valid");
+  }
+});
+const loginEmail = document.querySelector("#email-input");
+loginEmail.addEventListener("blur", e => {
+  fetch(`/api/users/login/${e.target.value}`)
+    .then(data => data.json())
+    .then(data => {
+      if (data.match == false) {
+        loginEmail.classList.add("is-valid");
+        loginEmail.classList.remove("is-invalid");
+      } else {
+        loginEmail.classList.remove("is-valid");
+        loginEmail.classList.add("is-invalid");
+      }
+    });
 });
