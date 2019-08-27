8c80bbf3b5093a6e3a91fca56c0217c79b9fc687
Aaron Keisler <amkeisler@gmail.com>
2019-08-26 23:50:35 -0400

    edit room display routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -12,24 +12,80 @@ module.exports = function(app) {
       req.flash("warning", "Unauthorized access.");
       res.redirect("/");
     } else if (req.user.isAdmin) {
-      const users = db.User.findAll().then(data => {
-        return data;
-      });
-      const rooms = db.Room.findAll().then(data => {
+      const rooms = db.Room.findAll({
+        include: [db.User, db.Reservation]
+      }).then(data => {
+        console.log(JSON.stringify(data));
         return data;
       });
       const reservations = db.Reservation.findAll().then(data => {
         return data;
       });
 
-      Promise.all([users, rooms, reservations]).then(data => {
-        ejsObj.users = data[0];
-        ejsObj.rooms = data[1];
+      const revenueData = db.User.findAll({
+        include: [
+          {
+            model: db.Room,
+            include: [
+              {
+                model: db.Reservation
+              }
+            ]
+          }
+        ]
+      }).then(data => {
+        console.log(JSON.stringify(data, null, 2));
+        return data;
+      });
+      Promise.all([revenueData, rooms, reservations]).then(data => {
+        // user revenue
+        const revenueEjs = data[0].map(user => {
+          const userRevArr = user.Rooms.map(room => {
+            return room.Reservations.reduce((acc, val) => {
+              acc += val.duration * parseInt(room.hourlyRate);
+              return acc;
+            }, 0);
+          });
+          const revenue = userRevArr.reduce((acc, val) => {
+            return acc + val;
+          }, 0);
+          const numReservations = user.Rooms.reduce((acc, val) => {
+            return acc + val.Reservations.length;
+          }, 0);
+          return {
+            id: user.id,
+            email: user.email,
+            rooms: user.Rooms.length,
+            reservations: numReservations,
+            revenue: "$ " + revenue.toFixed(2)
+          };
+        });
+
+        // reservations/room
+
+        const roomEjs = data[1].map(room => {
+          return {
+            id: room.id,
+            name: room.roomName,
+            type: room.roomType,
+            owner: room.User.email,
+            reservations: room.Reservations.length
+          };
+        });
+
+        ejsObj.users = revenueEjs;
+        ejsObj.rooms = roomEjs;
         ejsObj.reservations = data[2];
         return res.render("pages/admin", ejsObj);
       });
     }
   });
 
-  app.get("/admin/user/:id/delete", (req, res) => {});
+  app.get("/api/admin/user/:id/rooms", (req, res) => {
+    const rooms = db.Room.findAll({
+      where: {
+        userId: req.params.id
+      }
+    }).then(data => res.json(data));
+  });
 };
